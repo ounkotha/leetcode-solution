970cb94ff67ed25348a297fdd33818506516688d
Oli-Un-Nahar Asha <ounkotha@gmail.com>
2025-08-09 12:25:41 +0600

    Create AddTwoNumbersTwoClass.java

---
+++ leetcode/AddTwoNumbersTwoClass.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class AddTwoNumbersTwoClass {
+
+   
+  private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode nextNode = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextNode;
+        }
+        return prev;
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int total = val1 + val2 + carry;
+            carry = total / 10;
+
+            current.next = new ListNode(total % 10);
+            current = current.next;
+
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+        dummy =  reverseList(dummy.next);
+        return dummy;
+    }
+    }
